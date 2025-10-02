04ba35659cd1c3532c2fd198cfaed55dbf4600a7
fabian-pratama <fabiuss039@gmail.com>
2025-10-02 18:13:12 +0700

    Add files via upload

---
+++ FormProdukUtama.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ManajemenToko
+{
+    public partial class FormProdukUtama : Form
+    {
+        public FormProdukUtama()
+        {
+            InitializeComponent();
+        }
+
+        private void LoadDataProduk()
+        {
+            dgvProduk.Rows.Clear();
+            dgvProduk.Columns.Clear();
+            using (SqlConnection conn = Koneksi.GetConnection())
+            {
+                try
+                {
+                    conn.Open();
+                    string query = @"SELECT p.Id, p.NamaProduk, p.Harga, p.Stok, k.NamaKategori
+                    FROM Produk p
+                    LEFT JOIN Kategori k ON p.KategoriId = k.Id";
+
+                    SqlCommand cmd = new SqlCommand(query, conn);
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    // Tambahkan kolom ke grid
+                    dgvProduk.Columns.Add("Id", "ID");
+                    dgvProduk.Columns.Add("NamaProduk", "Nama Produk");
+                    dgvProduk.Columns.Add("Harga", "Harga");
+                    dgvProduk.Columns.Add("Stok", "Stok");
+                    dgvProduk.Columns.Add("Kategori", "Kategori");
+                    while (reader.Read())
+                    {
+                        dgvProduk.Rows.Add(
+                        reader["Id"],
+                        reader["NamaProduk"],
+                        reader["Harga"],
+                        reader["Stok"],
+                        reader["NamaKategori"]
+
+                        );
+                    }
+                    reader.Close();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Gagal menampilkan data: " + ex.Message);
+                }
+            }
+        }
+
+        private void FormProdukUtama_Load(object sender, EventArgs e)
+        {
+            LoadDataProduk();
+        }
+
+        private void btnTambah_Click(object sender, EventArgs e)
+        {
+            FormProdukDetail frm = new FormProdukDetail();
+            if (frm.ShowDialog() == DialogResult.OK)
+            {
+                LoadDataProduk(); // refresh data setelah tambah
+            }
+        }
+    }
+}
